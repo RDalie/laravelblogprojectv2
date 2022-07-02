6000ffc54a53cf88639c20b68b9869ab16a247be
RDalie <rabyatayal1201@gmail.com>
2022-07-02 18:35:41 +0530

    almost complete

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -17,7 +17,7 @@
                         <div class="portfolio-hover">
                             <div class="portfolio-hover-content"><i class="fas fa-plus fa-3x"></i></div>
                         </div>
-                        <img   class="img-fluid" src="/uploads/posts/{{$post->image}}"   alt="..." />
+                        <img  height="300px" width="360px" class="img-fluid" src="/uploads/posts/{{$post->image}}"   alt="..." />
                     </a>
                     <div class="portfolio-caption">
                         <div class="portfolio-caption-heading">{{$post->title}}</div>
@@ -26,6 +26,7 @@
                 </div>
             </div>
             @endforeach
+            
             {{-- <div class="col-lg-4 col-sm-6 mb-4">
                 <!-- Portfolio item 2-->
                 <div class="portfolio-item">
