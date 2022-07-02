648eba570354ae1b0eb455efed8eb98cb11f952c
RDalie <rabyatayal1201@gmail.com>
2022-07-02 15:11:42 +0530

    added changes

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/create', function(){
+    return view('create');
+});
+
+
+Route::get('/show', function(){
+    return view('show');
+});
+
+Route::get('/contact', function(){
+    return view('contact');
+});
